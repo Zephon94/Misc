51c9ab2caafcdc040d7c4fd34554075554b0043f
misc1170 <sloopy1170@gmail.com>
2022-06-09 22:33:30 +0300

    asg

---
+++ controllers/MyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class MyController extends Controller
+{
+public function actionIndex() {
+	return $this->render('index');
+}
+public function actionAbout() {
+	return $this->render('about');
+}
+public function actionService() {
+	return $this->render('service');
+}
+public function actionContact() {
+	return $this->render('contact');
+}
+}
---
+++ views/my/contact.php
@@ -0,0 +1,23 @@
+
+<div>
+    <img class="emblema" src="/web/images/Эмблема%20компании.jpg" width="100" height="100" style="margin-top: 60px; margin-left: -200px">
+</div>
+<div class="enumHeadPerson" style="font-size: 21px; margin-top: -100px">
+    <table>
+        <tr>
+            <td>Генеральный директор - Коломицев Сергей Игоревич<br>Телефон: 8-965-252-74-40 Mail: kolomitsev@asg.expert</td>
+            <td><img src="/web/images/Контакты/Рисунок6.png"></td>
+        </tr>
+        <tr>
+            <td> Технический директор - Клим Михаил Андреевич<br>Телефон: 8-925-888-23-24  Mail: klim@asg.expert</td>
+            <td><img src="/web/images/Контакты/Рисунок7.png"></td>
+        </tr>
+        <tr>
+            <td>Директор по развитию бизнеса - Машков Александр Сергеевич<br>Телефон: 8-926-593-26-74  Mail: mashkov@asg.expert</td>
+            <td><img src="/web/images/Контакты/Рисунок8.png"></td>
+        </tr>
+</div>
+<!--<div>-->
+<!--    <img src="/web/images/Контакты/Рисунок6.png" style="margin-top: 0px;margin-right: 10px">-->
+<!--</div>-->
+</table>
