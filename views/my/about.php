9e5fb64527fe5fcb74155abb05d592d61b9799c1
misc1170 <sloopy1170@gmail.com>
2022-06-11 19:12:25 +0300

    End About Page

--- views/my/about.php
+++ views/my/about.php
@@ -31,13 +31,27 @@
 	<div style="font-size: 20pt;font-weight: bold;">"Алгоритм Строй Групп"</div>
 	<div><img src="/web/images/Эмблема компании.jpg" style="height: 100px"></div>
 </div>
-<div style="display: inline-block;margin: 10px">
+<div style="margin: 0 10px 10px 0;">
 	<p><h2>С 2018 года на инженерно-строительном рынке</h2></p>
-	<ul>
+	<ul style="display: inline-block;">
 		<li>Большой спектр предоставляемых услуг</li>
 		<li>Опытная команда инженеров – стаж более 10 лет</li>
 		<li>Сотни счастливых клиентов</li>
 	</ul>
-	<img src="/web/images/Шестерёнки.jpg" style="display: inline-block;height: 35%;width: 35%;float: right;">
+	<img src="/web/images/Шестерёнки.jpg" style="height: 100px;">
+</div>
+<div style="float: left;">
+	<h2>Наши клиенты:</h2>
+	<div>
+		<img src="/web/images/Клиенты/Рисунок1.png" style="height: 100px;display: inline-block;">
+		<img src="/web/images/Клиенты/Рисунок2.png" style="height: 100px">
+	</div>
+	<div>
+		<img src="/web/images/Клиенты/Рисунок3.png" style="display: inline-block;height: 100px;margin-top: 60px;margin-left: 80px">
+		<img src="/web/images/Клиенты/Рисунок4.png" style="height: 200px">
+	</div>
+	<div>
+		<img src="/web/images/Клиенты/Рисунок5.png">
+	</div>
 </div>
  
