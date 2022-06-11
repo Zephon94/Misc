31569cb166fd657d42989c7a2418046bcf7904fe
misc1170 <sloopy1170@gmail.com>
2022-06-11 12:10:43 +0300

    Add About Page

--- views/my/about.php
+++ views/my/about.php
@@ -1,1 +1,43 @@
-
+<!-- <table style="margin-top: 55px;margin-left: 200px">
+	<tr>
+		<td style="font-size: 25pt; font-weight: bold;">"Алгоритм Строй Групп"</td>
+		<td><img src="/web/images/Эмблема компании.jpg" height="100px"></td>
+	</tr>
+</table>
+	<h2 style="font-size: 35pt">С 2018 года на инженерно-строительном рынке</h2>
+	<table style="font-size: 16pt; color: pink">
+		<tr>
+			<td>&#10148;Большой спектр предоставляемых услуг</td>
+			<td rowspan="3"><img src="/web/images/Шестерёнки.jpg" style="height: 200px"></td>
+		</tr>
+		<tr>
+			<td>&#10148;Опытная команда инженеров – стаж более 10 лет</td>
+		</tr>
+		<tr>
+			<td>&#10148;Сотни счастливых клиентов</td>
+		</tr>
+	</table>
+	<table style="margin-left: -100px">
+		<tr>
+			<td style="font-size: 40pt">Наши клиенты:</td>
+			<td rowspan="2"; valign="margin-right"><img src="/web/images/Клиенты/Рисунок2.png" style="width: 30%;height: 30%;margin-right: 100px"></td>
+		</tr>
+		<tr>
+			<td><img src="/web/images/Клиенты/Рисунок1.png"; style="width: 60%"></td>
+		</tr>
+	</table>
+ -->
+ <div class="div1" style="display: flex;align-items: center;justify-content: center;">
+	<div style="font-size: 20pt;font-weight: bold;">"Алгоритм Строй Групп"</div>
+	<div><img src="/web/images/Эмблема компании.jpg" style="height: 100px"></div>
+</div>
+<div style="display: inline-block;margin: 10px">
+	<p><h2>С 2018 года на инженерно-строительном рынке</h2></p>
+	<ul>
+		<li>Большой спектр предоставляемых услуг</li>
+		<li>Опытная команда инженеров – стаж более 10 лет</li>
+		<li>Сотни счастливых клиентов</li>
+	</ul>
+	<img src="/web/images/Шестерёнки.jpg" style="display: inline-block;height: 35%;width: 35%;float: right;">
+</div>
+ 
